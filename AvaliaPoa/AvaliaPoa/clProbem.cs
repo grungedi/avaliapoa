10fc5ea797352eb1ce67dad31983f2944f268be1
diego <diego.ti@outlook.com>
2015-11-29 20:59:32 -0200

    11 - Diego

---
+++ AvaliaPoa/AvaliaPoa/clProbem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace AvaliaPoa
+{
+	public class clProbem
+	{
+
+        private int codProblem;
+        private string Description;
+        private string Photo;
+        private string Address;
+        private int codSubCategory;
+
+        public int pcodProblem { get; set; }
+        public string pDescription { get; set; }
+        public string pPhoto { get; set; }
+        public string pAddress { get; set; }
+        public string pcodSubCategory { get; set; }
+
+        public void InsertProblem(string Description, string Address, string Photo, int CodSubCategory,int codNeighborhood)
+        {
+            clDataAccess InsertData = new clDataAccess();
+            SqlDataReader Insert = InsertData.ReturnDataset("insert into TB_Problem (Description,Address,Photo,CodSubCategory,codNeighborhood) values ( '"+Description+"','"+Address+"','"+Photo+"',"+codSubCategory+","+codNeighborhood+")");
+
+        }
+
+    }
+}
